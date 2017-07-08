ecb83f34c7a1d2cea3da65ba011f98388d7b9a18
Piotr Jaworski <piotr.m.jaworski@gmail.com>
2017-07-08 18:50:43 +0200

    Enabled CORS

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next();
+});
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
